08af31b505e807569ce735b93c3ef540d5edbbbf
duong-kiet <bintotet20041012@gmail.com>
2025-06-03 16:11:51 +0700

    feat: create new transaction

--- dev/test.js
+++ dev/test.js
@@ -2,7 +2,8 @@ const Blockchain = require("./blockchain");
 const bitcoin = new Blockchain();
 
 bitcoin.createNewBlock(2989, "11111", "22222");
-bitcoin.createNewBlock(2990, "22222", "33333");
+bitcoin.createNewTransaction(100, "ASKD", "FHJK");
 bitcoin.createNewBlock(2991, "33333", "44444");
+bitcoin.createNewTransaction(200, "FSKD", "YUIT");
 
 console.log(bitcoin);
