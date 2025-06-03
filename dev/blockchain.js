ef957fc3c3522458a93a5ad66000412e26349ca1
duong-kiet <bintotet20041012@gmail.com>
2025-06-03 16:19:36 +0700

    feat: hash block method

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -45,4 +45,14 @@ Blockchain.prototype.createNewTransaction = function (
   return this.getLastBlock()["index"] + 1;
 };
 
+Blockchain.prototype.hashBlock = function (
+  previousBlockHash,
+  currentBlockData,
+  nonce
+) {
+  const dataAsString =
+    previousBlockHash + nonce.toString() + JSON.stringify(currentBlockData);
+  const hash = sha256(dataAsString);
+};
+
 module.exports = Blockchain;
